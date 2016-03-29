9a1098db86025cea5291fde0a19442b154cfd767
Florin Vasilica <floryn.euro.2007@gmail.com>
2016-03-29 13:48:18 +0300

    ...

--- lab 21.03.2016/lab 21.03.2016/Program.cs
+++ lab 21.03.2016/lab 21.03.2016/Program.cs
@@ -9,12 +9,16 @@ namespace lab_21._03._2016
 {
     class Program
     {
+
+        static string continut = string.Empty;
+
+        static StreamReader sr = null;
+        static StreamWriter sw = null;
+
+
         static void Main(string[] args)
         {
-
-
-            StreamReader sr = null;
-            StreamWriter sw = null;
+            DirectoryInfo di = null;
 
             #region citire si scriere in fisier
             //try
@@ -43,7 +47,7 @@ namespace lab_21._03._2016
             //    Console.WriteLine(ex);
             //}
 
-            //Console.WriteLine("Scriemin fisier ");
+            //Console.WriteLine("Scriem in fisier ");
 
             //try
             //{
@@ -56,7 +60,7 @@ namespace lab_21._03._2016
             //            linie = Console.ReadLine();
             //        }
 
-            //        sw.Close();
+            //        
             //    }
             //}
             //catch (Exception ex)
@@ -68,31 +72,62 @@ namespace lab_21._03._2016
             #endregion
 
 
-            DirectoryInfo di = null;
-
             string numeDirector = Console.ReadLine();
 
             try
             {
+                di = new DirectoryInfo(numeDirector);
+                DirectoryInfo[] arrDir = di.GetDirectories();
                 DirectoryInfo dirPrograms = new DirectoryInfo(numeDirector);
                 FileInfo fi = new FileInfo(numeDirector);
-                di = new DirectoryInfo(numeDirector);
+                var primulDir = arrDir[0];
 
                 Console.WriteLine("Enumerare directoare:");
-                var dirs = from dir in dirPrograms.EnumerateDirectories()
+                var dirs = from dir in primulDir.EnumerateDirectories()
 
                            select new
                            {
                                ProgDir = dir,
                            };
 
-                foreach (var d in dirs)
+
+
+                //scriere continut in fisier
+
+                try
                 {
-                    Console.WriteLine("{0}", d.ProgDir.Name);
+                    using (sw = new StreamWriter("output.txt"))
+                    {
+                        sw.Write(continut);
+                    }
+
                 }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+                //afisareSubdirectoare(di, 0);
+
+
+
+                //using (sw = new StreamWriter("output.txt"))
+                //    {
+                //        string linie = Console.ReadLine();
+                //        while (linie != "exit")
+                //        {
+                //            sw.Write(linie + Environment.NewLine);
+                //            linie = Console.ReadLine();
+                //        }
+
+                //        
+                //    }
+
+
                 Console.WriteLine("*********************");
                 Console.WriteLine("Enumerare fisiere:");
-                var files = from f in dirPrograms.EnumerateFiles()
+                var files = from f in primulDir.EnumerateFiles()
 
                             select f;
 
@@ -102,11 +137,9 @@ namespace lab_21._03._2016
                 }
 
 
-                
+
                 Console.WriteLine("*********************");
-
-
-                Console.WriteLine("Nume complet: {0}, \nData creatiei: {1}", di.FullName, di.CreationTime);
+                Console.WriteLine("Nume complet: {0} \nData creatiei: {1}", di.FullName, di.CreationTime);
                 Console.WriteLine("Numarul de directoare: {0}", di.GetDirectories().Count());
                 Console.WriteLine("Numarul de fisiere: {0}", di.GetFiles().Count());
                 Console.WriteLine("Numarul de fisiere .zip: {0}", di.GetFiles("*.zip").Count());
@@ -118,8 +151,61 @@ namespace lab_21._03._2016
                 Console.WriteLine(ex);
             }
 
-
             Console.ReadKey();
         }
+
+        static void afisareSubdirectoare(DirectoryInfo diDeAfisat, int nivel)
+        {
+
+            string listaFP = string.Empty;
+
+            foreach (var f in diDeAfisat.GetDirectories())
+            {
+
+                string minusuri = null;
+                for (int i = 0; i < nivel; i++)
+                {
+                    minusuri += "-";
+                }
+
+                //Console.WriteLine(minusuri + " " + f.Name);
+                continut += minusuri + " " + f.Name + Environment.NewLine;
+
+                foreach (var di in f.GetDirectories())
+                {
+                    listaFP += "dir: " + di.Name + Environment.NewLine;
+                }
+
+
+                try
+                {
+                    using (sw = new StreamWriter(f.FullName + Path.DirectorySeparatorChar + "xxx.txt"))
+                    {
+                        sw.Write(listaFP);
+                    }
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+                foreach (var fInfo in f.GetFiles())
+                {
+                    //Console.WriteLine(fInfo.Length + " " + fInfo.Name);
+                    continut += ">>>" + fInfo.Name + Environment.NewLine;
+                }
+
+
+                afisareSubdirectoare(f, nivel + 1);
+            }
+
+
+
+        }
+
+
+
     }
 }
