cd4813a81dc0701972f81459ec3705bd9831d81e
Florin Vasilica <floryn.euro.2007@gmail.com>
2016-04-04 16:23:44 +0300

    curs

--- lab 04.04.2016/lab 04.04.2016/MainWindow.xaml.cs
+++ lab 04.04.2016/lab 04.04.2016/MainWindow.xaml.cs
@@ -24,5 +24,65 @@ namespace lab_04._04._2016
         {
             InitializeComponent();
         }
+
+        private void btn_afiseaza_Click(object sender, RoutedEventArgs e)
+        {
+            //lbl_mesaj.Content = "text scris alabala portocala";
+            //lbl_mesaj.Content = textBox.Text;
+
+            //lbl_mesaj.Content += string.Format("Prelucrare date: {0}; Citit regulament: {1}", checkBox1.IsChecked, checkBox2.IsChecked);
+
+            lbl_mesaj.Content += string.Format("{0} cu valoarea {1}\n{2} cu valoarea {3}", r1.Content, r1.IsChecked, r2.Content, r2.IsChecked);
+            lbl_mesaj1.Content += string.Format("{0} cu valoarea {1}\n{2} cu valoarea {3}", r3.Content, r3.IsChecked, r4.Content, r4.IsChecked);
+
+
+
+
+
+        }
+
+        private void btn_sterge_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult mbr = MessageBox.Show("Esti sigur?", "Atentie", MessageBoxButton.YesNo);
+            if (mbr == MessageBoxResult.Yes)
+            {
+                lbl_mesaj.Content = String.Empty;
+                textBox.Text = String.Empty;
+
+            }
+        }
+
+       
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            if (comboBox.SelectedValue is Button)
+            {
+                lbl_mesaj.Content += "Am slectat din combobox: " + (comboBox.SelectedValue as Button).Content;
+            }
+            else if (comboBox.SelectedValue is ComboBoxItem)
+            {
+                lbl_mesaj.Content += "Am slectat din combobox: " + (comboBox.SelectedValue as ComboBoxItem).Content;
+            }
+            else if (comboBox.SelectedValue is CheckBox)
+            {
+                lbl_mesaj.Content += "Am slectat din combobox: " + (comboBox.SelectedValue as CheckBox).IsChecked + (comboBox.SelectedValue as CheckBox).Content;
+            }
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var mbr = MessageBox.Show("Salvam datele", "Atentie", MessageBoxButton.OKCancel);
+            if (mbr == MessageBoxResult.OK)
+            {
+                wnd1.Title = "salvare date...";
+            }
+            else
+            {
+                wnd1.Title = "nu se salveaza...";
+            }
+        }
     }
 }
