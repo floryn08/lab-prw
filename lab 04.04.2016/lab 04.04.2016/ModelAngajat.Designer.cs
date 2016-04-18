dbddc9a27cb2affa7ae8bd29187f7362d84f5875
Florin Vasilica <floryn.euro.2007@gmail.com>
2016-04-18 15:53:57 +0300

    curs 18.04.2016

---
+++ lab 04.04.2016/lab 04.04.2016/ModelAngajat.Designer.cs
@@ -0,0 +1,10 @@
+﻿// T4 code generation is enabled for model 'E:\Flow\Facultate\PRW_Sem4\Git\lab-prw\lab 04.04.2016\lab 04.04.2016\ModelAngajat.edmx'. 
+// To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
+// property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
+// is open in the designer.
+
+// If no context and entity classes have been generated, it may be because you created an empty model but
+// have not yet chosen which version of Entity Framework to use. To generate a context class and entity
+// classes for your model, open the model in the designer, right-click on the designer surface, and
+// select 'Update Model from Database...', 'Generate Database from Model...', or 'Add Code Generation
+// Item...'.
---
+++ lab 04.04.2016/lab 04.04.2016/Window3.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab_04._04._2016
+{
+    /// <summary>
+    /// Interaction logic for Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void btnStergere_Click_1(object sender, RoutedEventArgs e)
+        {
+            txtCodDep.Text = txtRezultat.Text = String.Empty;
+        }
+
+        private void btnAfisare_Click(object sender, RoutedEventArgs e)
+        {
+            int codDep = Convert.ToInt32(txtCodDep.Text);
+            var DE = new Database1Entities();
+
+            var rezultat = (from x in DE.Angajats
+                            where x.CodDep == codDep
+                            select x).ToList();
+
+            foreach (var item in rezultat)
+            {
+                txtRezultat.Text += item.Nume + " " + item.Marca + " " + item.Email + " " + item.Salariu + " " + item.Departament.Denumire;
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //int codDep = Convert.ToInt32(txtCodDep.Text);
+            var DE = new Database1Entities();
+
+            var rez = (from x in DE.Departaments
+                       select x).ToList();
+
+            foreach (var item in rez)
+            {
+                txtRezultat.Text += item.Denumire + " ";
+            }
+
+
+        }
+    }
+}
